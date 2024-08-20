31715c88b7daa8eb776d821262b8119b7fd631da
networksaphyra <networksaphyra004@gmail.com>
2024-08-20 00:58:14 -0400

    feat: added popup, improved press section, renamed hero button.

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ const fadeInSections = () => {
   $("section").addClass("show");
 }
 
+const setupPopup = () => {
+  const $newsletterButton = $("#newsletter-button");
+  const $popup = $(".popup");
+  const $overlay = $(".overlay");
+  const $closePopup = $("#closePopup");
+
+  $newsletterButton.on("click", () => {
+    $popup.fadeIn(300);
+    $overlay.fadeIn(300);
+  });
+
+  $closePopup.on("click", () => {
+    $popup.fadeOut(300);
+    $overlay.fadeOut(300);
+  });
+
+  $overlay.on("click", () => {
+    $popup.fadeOut(300);
+    $overlay.fadeOut(300);
+  });
+}
+
 const main = () => {
   starAnimation();
 
@@ -11,8 +33,9 @@ const main = () => {
     $("body").css("overflow", "visible");
     fadeInSections();
   }, 1000);
+
+  setupPopup();
 }
 
 
 $(document).ready(main);
-
